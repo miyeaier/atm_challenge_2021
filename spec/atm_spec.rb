f35eb404576fea97873267836c6dce2af4036507
miyeaier <miyesier@gmail.com>
2021-11-14 21:31:32 +0100

    add test on atm spec and difined method withdrew in atm.rb,and test go green

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -5,4 +5,9 @@ describe Atm do
   it 'is expected to have $1000 when instantiated' do
     expect(subject.funds).to eq 1000
   end
+
+  it 'is expected to reduce funds on withdraw' do
+    subject.withdraw 50
+    expect(subject.funds).to eq 950
+  end
 end
