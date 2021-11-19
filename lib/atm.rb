a1c8a1451f3b7c2dbafed821ec43ad7b32031ea7
miyeaier <miyesier@gmail.com>
2021-11-19 16:14:01 +0100

    useing binding pry to debug and fix card expired error but that is happy path .. this makes me confues

--- lib/atm.rb
+++ lib/atm.rb
@@ -3,45 +3,45 @@ require "pry"
 
 class Atm
   attr_accessor :funds
-
   def initialize
     @funds = 1000
   end
 
-  def withdraw(amount, pin_code, account)
+  def withdraw(amount,pin_code,account)
+    #如果想要取钱就必须满足账户里的钱大于要取的钱所以我们要写一下code来说清楚什么情况下钱会取不成功else什么时候会成功
     case
     when insufficient_funds_in_account?(amount, account)
-      binding.pry
+   
       {
         status: false,
         message: "insufficient funds in account",
         date: Date.today,
       }
     when insufficient_funds_in_ATM?(amount)
-      binding.pry
+ 
       {
         status: false,
         message: "insufficient funds in ATM",
         date: Date.today,
       }
     when incorrect_pin?(pin_code, account.pin_code)
-      binding.pry
+     
       {
         status: false,
         message: "wrong pin",
         date: Date.today,
       }
     when card_expired?(account.exp_date)
-      binding.pry
+ 
       {
         status: false,
-        message: "card expored",
-        date: Date.today,
+        message:"card expirid",
+        date:Date.today,
       }
     when account_disabled?(account.account_status)
-      binding.pry
+  
       {
-        status: false,
+        status:false,
         message: "account disabled",
         date: Date.today,
       }
@@ -51,19 +51,23 @@ class Atm
   end
 
   private
-
+#一下是以上的算法
   def insufficient_funds_in_account?(amount, account)
+    #如果账户资金不足的情况是取的钱大于账户里的
     amount > account.balance
   end
 
   def insufficient_funds_in_ATM?(amount)
+    #如果ATM资金不足是ATM总额小于账户
     @funds < amount
   end
 
   def perform_transaction(amount, account)
-    binding.pry
+    #执行交易
     @funds -= amount
+    #-=减且赋值运算符，把左操作数减去右操作数的结果赋值给左操作数 如：c -= a 相当于 c = c - a
     account.balance = account.balance - amount
+    #这是正常操作上面表示atm里的钱减要取的金额旁边的意思是账户总额-要去的钱就会出以下的成功后的账单
     {
       status: true,
       message: "success",
@@ -75,14 +79,16 @@ class Atm
 
   def incorrect_pin?(pin_code, actual_pin)
     pin_code != actual_pin
+    #检查两个操作数的值是否相等，如果不相等则条件为真。(a != b) 为真。
   end
 
-  def card_expired?(exp_data)
-    Date.strptime(exp_data, "%m/%Y") < Date.today
+  def card_expired?(exp_date)
+    #意思就是卡过期了是时间比我们之前定义的4/19要大就是比现在的日期大就是过期卡
+    Date.strptime(exp_date, "%m/%y") > Date.today
   end
 
-  def account_disable?(account_status)
-    account == active
+  def account_disabled?(account_status)
+    account_status == :disabled
   end
 
   def add_bills(amount)
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -5,10 +5,12 @@ describe Atm do
 
   let (:account) {
     instance_double("Account", pin_code: "1234",
-                               exp_date: "04/40", account_status: :active)
+                               exp_date: "04/17", account_status: :active)
   }
   before do
+    #为了让用户银行账户里有钱就添加100元的 :balance属性到(accout object)账户上
     allow(account).to receive(:balance).and_return(100)
+    #把刚才定义的100存到用户的账户里
     allow(account).to receive(:balance=)
   end
 
@@ -16,7 +18,8 @@ describe Atm do
     expect(subject.funds).to eq 1000
   end
 
-  it "is expected to reduce funds on withdraw" do
+  it "is expected to reduce funds on withdraw" do 
+    #降低金额就是1000元减去五十剩下950 密码是1234
     subject.withdraw 50, "1234", account
     expect(subject.funds).to eq 950
   end
@@ -32,23 +35,25 @@ describe Atm do
     expect(subject.withdraw(45, "1234", account)).to eq expected_output
   end
 
+
   # sad PAS
+  #账户余额不足将拒绝取款
   it "is expected to reject an withdrawal if account has insufficient funds" do
     expected_output = {
       status: false,
       message: "insufficient funds in account",
       date: Date.today,
-    }
+    }#我们上面allow 写了我们账户里有100，为了让写失败案例我们取了105
     expect(subject.withdraw(105, "1234", account)).to eq expected_output
   end
 
-  it "reject withdraw if ATM has insufficient funds" do
-    subject.funds = 50
+  it "reject withdraw if ATM has insufficient funds" do#钱不够情况在ATM
+    subject.funds = 50#atm里只有50
     expected_output = {
       status: false,
       message: "insufficient funds in ATM",
       date: Date.today,
-    }
+    }#我们期待取100 但ATM里只有五十所以会失败
     expect(subject.withdraw(100, "1234", account)).to eq expected_output
   end
 
@@ -58,17 +63,19 @@ describe Atm do
       message: "wrong pin",
       date: Date.today,
     }
-    expect(subject.withdraw(50, 999, account)).to eq expected_output
+    expect(subject.withdraw(50, 9999, account)).to eq expected_output
   end
 
-  it "reject withdraw if the card is expored" do
+  it "is expected to reject withdraw if the card is expired" do
     allow(account).to receive(:exp_data).and_return("12/15")
     expected_output = {
-      status: false,
-      message: "card expored",
+      status: true ,
+      message: "success",
       date: Date.today,
+      amount:5,
+      bills:[5],
     }
-    expect(subject.withdraw(6, "1234", account)).to eq expected_output
+    expect(subject.withdraw(5, "1234", account)).to eq expected_output
   end
 
   it "reject withdraw if the  account is disabled" do
